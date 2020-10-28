1d2a29f2d9b7f61d7d0da64c417b380ac1e6c242
Andreas Wilson <andreasw@gmail.com>
2020-10-28 09:10:02 +0100

    Changed to colums in bootstrap

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import React, { CSSProperties } from 'react';
 import './MovieCard.scss';
 import { Movie, toggleFavoriteMovie, updateMovie } from '../actions';
+import { Col } from 'reactstrap';
 
 interface Props {
   movie: Movie;
@@ -20,44 +21,46 @@ export const MovieCard: React.FC<Props> = ({
   };
 
   return (
-    <a href={'movie/' + movie._id}>
-      <div className='movie_card' style={backgroundCSS}>
-        <div className='info_section'>
-          <div className='movie_header'>
-            <img className='locandina' src={movie.Poster} alt={movie.Title} />
-            <h1>{movie.Title}</h1>
-            <h4>{movie.Year}</h4>
-            {movie.Runtime !== 'N/A' && (
-              <span className='minutes'>{movie.Runtime}</span>
-            )}
-            <p className='type'>{movie.Genre}</p>
+    <Col md='12' lg='6'>
+      <a href={'movie/' + movie._id}>
+        <div className='movie_card' style={backgroundCSS}>
+          <div className='info_section'>
+            <div className='movie_header'>
+              <img className='locandina' src={movie.Poster} alt={movie.Title} />
+              <h1>{movie.Title}</h1>
+              <h4>{movie.Year}</h4>
+              {movie.Runtime !== 'N/A' && (
+                <span className='minutes'>{movie.Runtime}</span>
+              )}
+              <p className='type'>{movie.Genre}</p>
+            </div>
+
+            <div className='movie_desc'>
+              <p className='text'>{movie.Plot}</p>
+            </div>
+
+            <div className='movie_social'>
+              <ul>
+                <li
+                  onClick={(event: React.MouseEvent<HTMLElement>) => {
+                    event.preventDefault(); // disable href on favorite
+                    toggleFavoriteMovie(movie._id);
+                    updateMovie(movie);
+                  }}
+                >
+                  {movie.Favorite ? (
+                    <i className='fas fa-heart'></i>
+                  ) : (
+                    <i className='far fa-heart'></i>
+                  )}
+                </li>
+              </ul>
+            </div>
           </div>
 
-          <div className='movie_desc'>
-            <p className='text'>{movie.Plot}</p>
-          </div>
-
-          <div className='movie_social'>
-            <ul>
-              <li
-                onClick={(event: React.MouseEvent<HTMLElement>) => {
-                  event.preventDefault(); // disable href on favorite
-                  toggleFavoriteMovie(movie._id);
-                  updateMovie(movie);
-                }}
-              >
-                {movie.Favorite ? (
-                  <i className='fas fa-heart'></i>
-                ) : (
-                  <i className='far fa-heart'></i>
-                )}
-              </li>
-            </ul>
-          </div>
+          <div className='blur_back'></div>
         </div>
-
-        <div className='blur_back'></div>
-      </div>
-    </a>
+      </a>
+    </Col>
   );
 };
